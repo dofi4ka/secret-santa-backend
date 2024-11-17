3c2e4537777b967fa2b4fa613558c2861dadd473
dofi4ka <dofi4ka@yandex.ru>
2024-11-17 22:09:49 +0300

    changed default value of ACCESS_TOKEN_EXPIRE_MINUTES from 30 to 60*60*24

--- app/core/security.py
+++ app/core/security.py
@@ -15,7 +15,7 @@ auth_router = APIRouter()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30)
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60*60*24)
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
